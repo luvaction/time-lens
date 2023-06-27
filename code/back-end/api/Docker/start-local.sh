b6b46dfa99a6a5dcc9e3c86deccc6006ea00f2be
jyk <luvaction0@gmail.com>
2023-06-27 16:28:04 +0900

    Chore: Garbage Clean

--- code/back-end/api/Docker/start-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_NAME=my
-
-docker-compose -p $PROJECT_NAME -f docker-compose.local.yml down --volumes
-rm -rf ../database/volumes/local/db/data
-rm -rf ../database/volumes/local/db/backup
-docker-compose -p $PROJECT_NAME -f docker-compose.local.yml up -d --build
-# docker-compose -p $PROJECT_NAME -f docker-compose.local.yml up -d --build --force-recreate
