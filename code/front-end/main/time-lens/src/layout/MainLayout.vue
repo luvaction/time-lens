f05330c70cbe69de00b114690c8c1caa3b574a4a
JYK <luvaction0@gmail.com>
2023-06-30 14:34:06 +0900

    Feat: Install SCSS Package

--- code/front-end/main/time-lens/src/layout/MainLayout.vue
+++ code/front-end/main/time-lens/src/layout/MainLayout.vue
@@ -25,37 +25,35 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$layout-bg-color: rgb(160, 160, 160);
+$footer-bg-color: rgb(48, 48, 48);
+$sidebar-bg-color: rgb(102, 72, 28);
+$main-bg-color: rgb(214, 214, 214);
+$default-font-size: 28px;
 .layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  overflow: auto; /* 이 코드가 추가되었습니다 */
+  overflow: auto;
 }
 
 .header {
   flex: 0 0 10%;
-  font-size: 30px;
+  font-size: $default-font-size;
   max-height: 10%;
-  background-color: rgb(
-    177,
-    177,
-    177
-  ); /* 배경색이 잘 적용되었는지 확인하실 수 있도록 추가했습니다 */
+  background-color: $layout-bg-color;
 }
+
 .footer {
   flex: 0 0 10%;
-  font-size: 20px;
+  font-size: $default-font-size;
   max-height: 10%;
-  background-color: rgb(
-    48,
-    48,
-    48
-  ); /* 배경색이 잘 적용되었는지 확인하실 수 있도록 추가했습니다 */
+  background-color: $footer-bg-color;
 }
 
 .main {
-  font-size: 30px;
+  font-size: $default-font-size;
   display: flex;
   flex: 1;
 }
@@ -63,12 +61,11 @@ export default defineComponent({
 .sidebar {
   flex: 0 0 20%;
   max-width: 20%;
-  background-color: green; /* 배경색이 잘 적용되었는지 확인하실 수 있도록 추가했습니다 */
+  background-color: $sidebar-bg-color;
 }
 
-/* 이 CSS는 router-view에 배경색을 추가합니다. */
 .main > *:last-child {
   flex: 1;
-  background-color: rgb(214, 214, 214);
+  background-color: $main-bg-color;
 }
 </style>
